bca2bfad3f1616e4061c7ab161e216af4a799abb
whs <hswongac@gmail.com>
2024-02-02 23:07:16 +0800

    Test

--- candle-kernels/build.rs
+++ candle-kernels/build.rs
@@ -4,6 +4,6 @@ fn main() {
     let builder = bindgen_cuda::Builder::default();
     println!("cargo:info={builder:?}");
     let bindings = builder.build_ptx().unwrap();
-    let out_path = std::path::PathBuf::from(env::var("OUT_DIR").unwrap());
+    let out_path = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
     bindings.write(out_path.join("lib.in")).unwrap();
 }
